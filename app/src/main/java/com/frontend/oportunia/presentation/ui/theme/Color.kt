6bca8ea0e99b11518eca426589ec7cff1b3b375f
Ana <ana.avendano.ramirez@est.una.ac.cr>
2025-03-24 17:32:13 -0600

    Initial setup and design elements

--- app/src/main/java/com/frontend/oportunia/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/frontend/oportunia/presentation/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.frontend.oportunia.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Primary50 = Color(0xFFE7E5FF)
+val Primary100 = Color(0xFFCCC8FF)
+val Primary200 = Color(0xFF9A91FF)
+val Primary300 = Color(0xFF8075FF)
+val Primary400 = Color(0xFF665ECC)
+val Primary500 = Color(0xFF4D4699)
+val Primary900 = Color(0xFF0D0C19)
--- app/src/main/java/com/frontend/oportunia/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/frontend/oportunia/presentation/ui/theme/Theme.kt
@@ -8,35 +8,42 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Primary400,
+    onPrimary = Primary50,
+    secondary = Primary300,
+    onSecondary = Primary50,
+    primaryContainer = Primary100,
+    onPrimaryContainer = Primary900,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = Primary50,
+    onBackground = Primary900,
+
+    surface = Primary50,
+    onSurface = Primary900,
 )
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Primary200,
+    onPrimary = Primary900,
+    primaryContainer = Primary500,
+    onPrimaryContainer = Primary50,
+
+    background = Primary900,
+    onBackground = Primary50,
+
+    surface = Primary500,
+    onSurface = Primary50,
+)
+
 
 @Composable
 fun OportunIATheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -52,6 +59,7 @@ fun OportunIATheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = content,
+        shapes = Shapes
     )
 }
