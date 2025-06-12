73a2a91b48b34e9823da8daa64b85cee2922921b
Ana <ana.avendano.ramirez@est.una.ac.cr>
2025-06-12 03:14:48 -0600

    Fix

--- app/src/main/java/com/frontend/oportunia/data/remote/interceptor/AuthInterceptor.kt
+++ app/src/main/java/com/frontend/oportunia/data/remote/interceptor/AuthInterceptor.kt
@@ -17,9 +17,7 @@ class AuthInterceptor @Inject constructor(
 
         // Skip authentication for login and registration endpoints
         if (originalRequest.url.encodedPath.contains("auth/login") ||
-            originalRequest.url.encodedPath.contains("auth/register") ||
-            originalRequest.url.encodedPath.contains("/users") ||
-            originalRequest.url.encodedPath.contains("/students")
+            originalRequest.url.encodedPath.contains("auth/register")
         ) {
             return chain.proceed(originalRequest)
         }
